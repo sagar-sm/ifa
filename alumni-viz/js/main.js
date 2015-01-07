24e085606ac679b6af751f092ee2966af4e00ea8
staff <staff@172-16-217-79.DYNAPOOL.NYU.EDU>
2015-01-06 19:57:52 -0500

    fixed av-style for map

--- alumni-viz/js/main.js
+++ alumni-viz/js/main.js
@@ -1,31 +1,31 @@
 window.onload = function(){
-  var width = 960,
+  var width = 940,
     height = 480;
 
   var projection = d3.geo.equirectangular()
-      .scale(153)
-      .translate([width / 2, height / 2])
-      .precision(.1);
+    .scale(153)
+    .translate([width / 2, height / 2])
+    .precision(.1);
 
   var path = d3.geo.path()
-      .projection(projection);
+    .projection(projection);
 
   var graticule = d3.geo.graticule();
 
   var svg = d3.select("#viz").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+    .attr("width", width)
+    .attr("height", height);
 
   svg.append("path")
-      .datum(graticule)
-      .attr("class", "graticule")
-      .attr("d", path);
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
 
   d3.json("./data/world-110m.json", function(error, world) {
     svg.insert("path", ".graticule")
-        .datum(topojson.feature(world, world.objects.land))
-        .attr("class", "land")
-        .attr("d", path);
+      .datum(topojson.feature(world, world.objects.land))
+      .attr("class", "land")
+      .attr("d", path);
 
     svg.insert("path", ".graticule")
         .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
